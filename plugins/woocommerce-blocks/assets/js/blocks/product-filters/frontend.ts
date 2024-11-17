ee005e599919c12daac728e28f66e0c3cd8f1b3a
Tung Du <dinhtungdu@gmail.com>
2024-11-17 12:43:08 +0700

    refactor: remove export for navigate

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/frontend.ts
@@ -37,7 +37,7 @@ function isParamsEqual(
 	return true;
 }
 
-export function navigate( href: string, options = {} ) {
+function navigate( href: string, options = {} ) {
 	/**
 	 * We may need to reset the current page when changing filters.
 	 * This is because the current page may not exist for this set
